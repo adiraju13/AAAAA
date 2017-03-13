cb095ff24d3cb328e77a999807dd5f8d4a8891cb
adiraju13 <adityaraju@Adityas-MacBook-Pro-3.local>
2017-03-13 01:55:27 -0700

    last fix to the make deploy. no need for review we all reviewed it together

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,8 +1,8 @@
 #kills the running process, and creates a new web-server to run
 docker kill "$(docker ps -q)"
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -a -q)
+docker stop "$(docker ps -a -q)"
+docker rm "$(docker ps -a -q)"
+docker rmi "$(docker images -a -q)"
 docker load -i webserver_img
 docker run -d -p 80:2020 httpserver:latest ./web-server simple_config
 echo build successfully deployed
